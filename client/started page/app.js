7a0e0c9b57650ae892ff6b5c66360ae593f4861c
evakuntsevich19 <evakuntsevich1906@gmail.com>
2022-09-18 22:04:10 +0300

    button for started page

---
+++ client/started page/app.js
@@ -0,0 +1,15 @@
+const startBtn = document.querySelector(`.start`);
+const registrBtn = document.querySelector(`.registr`);
+const loginBtn = document.querySelector(`.login`);
+
+startBtn.addEventListener("click", () => {
+    window.location.href = 'file:///C:/Users/HP/Desktop/task-manager/client/client%20auth/index.html';
+});
+
+registrBtn.addEventListener("click", () => {
+    window.location.href = 'file:///C:/Users/HP/Desktop/task-manager/client/client%20registr/index.html';
+});
+
+loginBtn.addEventListener("click", () => {
+    window.location.href = 'file:///C:/Users/HP/Desktop/task-manager/client/client%20registr/index.html';
+})
--- server/src/api/api.repository.js
+++ server/src/api/api.repository.js
@@ -108,6 +108,7 @@ module.exports = {
     getAllTaskDB,
     createUserDB,
     createTaskDB,
+    authUserDB,
     updateTaskByIDDB,
     deleteTaskByIDDB
 };
